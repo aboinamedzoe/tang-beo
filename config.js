284d2f928b59ac383520f80a3f8591a7366409e5
aboinamedzoe <87515241+aboinamedzoe@users.noreply.github.com>
2021-07-16 13:10:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,15 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Hehe chào trứng ngok',
+    introDesc: `Hong biết cậu có chút hong thoải mái nào hong nma cứ chơi cho tớ zui nha`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Nghe nói hum nay có người có mụt ngày bận rộn😙',
+    desc: 'Cậu có mún vừa nghỉ ngơi vừa nch với tớ hem ',
+    btnYes: 'Cóaaaaa',
+    btnNo: 'Không, mơ đi ',
+    question:'Quick question nè,điều cậu thít nhất ở tớ',
+    btnReply: 'THẬt LÒNG NGHEN <3',
+    reply: 'Đượttt <33333333',
+    mess: 'Hehe bít ngay 🥰. Hubby của cậu mà lại',
+    messDesc: 'Last one,đừng quơn kèo giữa chúng ta nhóeee',
+    btnAccept: 'Dạaaaaa <3',
 }
