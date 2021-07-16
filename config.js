e4e95e717cdbd7756f5b08221f13235c7ab19fae
aboinamedzoe <87515241+aboinamedzoe@users.noreply.github.com>
2021-07-16 13:29:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     btnYes: 'Có chớ <3(có thì dr tớ nghen)',
     btnNo: 'Không,mơ đi :3',
     question:'Quick question nè,cậu thích điều gì nhất ở tớ',
-    btnReply: 'Gửi cho tớ nghen <3',
+    btnReply: 'Gửi cho tớ nghen,dù gì tớ cũng khum đọc được đâu nên đừng ngại <3',
     reply: 'Hmm cũng gọi là cute đới <33333333',
     mess: 'Hehe tớ biết mà 🥰. Hubby của cậu mà lại',
     messDesc: 'Nhớ đừng quên kèo giữa chúng ta nghennn.Chúc cậu có mụt buổi tối zui zẻ nhéee',
